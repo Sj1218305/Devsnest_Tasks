adc658508b08cb5dee141bd74aaa38b43a341244
sj1218305 <jainshobit0@gmail.com>
2021-06-25 23:57:22 +0530

    fix

--- index.js
+++ index.js
@@ -1,52 +1,39 @@
-let list = document.getElementById("todo-container");
+let container = document.getElementById("todo-container");
 
 function getTodos(){
   let todos = localStorage.getItem("todos");
-  console.log(todos);
-  list.innerHTML = todos;
+  console.log(todos)
+  container.innerHTML = todos;
 }
-
-function addTodo(e) {
+function addTodo(e){
   if(e.code === "Enter"){
-    todoInput = document.getElementById('todo-input');
-    let todo = todoInput.value;
-    if(todo){
-      let li= document.createElement("li");
-      let span = document.createElement("span");
-      let icon = document.createElement("i");
-      icon.classList.add("fa");
-      icon.classList.add("fa-trash");
-      span.appendChild(icon);
-      let todoNode = document.createTextNode(todo)
-      li.appendChild(todoNode);
-      li.insertBefore(span,todoNode);
-      document.getElementById("todo-container").appendChild(li);
-      localStorage.setItem("todos", list.innerHTML);
-      todoInput.value = "";
-    }
-    else{
-      alert("Please add a to-do");
-    }
+      let todo = document.getElementById('todo-input').value;
+      if(todo){
+        document.getElementById("todo-container").innerHTML += `<li><span><i class="fa fa-trash"></i></span>${todo}</li>`
+      document.getElementById('todo-input').value = "";
+      }else{
+        alert("Please enter some value");
+      }
   }
+  localStorage.setItem("todos", container.innerHTML);
 }
 
-document.getElementById("add-new").addEventListener('click', function(){
-  document.getElementById("todo-input").classList.toggle("add");
+document.getElementById("addNew").addEventListener("click", function(){
+  document.getElementById("todo-input").classList.toggle("showInput");
 })
 
 document.querySelector("ul").addEventListener("click", function(e){
-  if(event.target.tagName === "LI"){
-    e.target.classList.toggle("completed");
-  }
-  localStorage.setItem("todos", list.innerHTML);
+  e.target.classList.toggle("completed");
+  localStorage.setItem("todos", container.innerHTML);
 });
 
+
 document.querySelector("ul").addEventListener("click", function(e){
-  if(event.target.tagName === "I"){ 
-    event.target.parentElement.parentElement.remove();
-  }
-  if(event.target.tagName === "SPAN"){
-    event.target.parentElement.remove();
-  }
-  localStorage.setItem("todos", list.innerHTML);
+  if(e.target.tagName === "I")
+  e.target.parentElement.parentElement.remove();
+  else if(e.target.tagName === "SPAN")
+  e.target.parentElement.remove();
+  localStorage.setItem("todos", container.innerHTML);
 });
+
+  
